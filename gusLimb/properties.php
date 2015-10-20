595a23595f3827ad11813b0ef0b9d738fd2174c4
altapia <altapia@gmail.com>
2015-10-20 09:03:59 +0200

    Config files

---
+++ gusLimb/properties.php
@@ -0,0 +1,6 @@
+<?php
+    //Registrar la url del bot en Telegram (tiene que ser https)
+    //https://api.telegram.org/botTOKEN/setWebhook?url=<url>
+    $TOKEN='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+
+?>
