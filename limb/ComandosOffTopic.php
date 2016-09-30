dae0d36b8cfb6cc292d2999966644d0c40232951
Antonio Tapia <altapia@gmail.com>
2016-09-30 06:21:47 +0000

    fix sintax

--- limb/ComandosOffTopic.php
+++ limb/ComandosOffTopic.php
@@ -241,7 +241,7 @@
                     return Response::create_doc_response($endpoint, $request->get_chat_id(), $file_id);
                     break;
 		case 3:
-                    $file_id= Utils::aleatorio(array('AgADBAADyrExG6uCfgAByJa4e096PDrUuqYwAAT4WookikriBOAAIC', 'AgADBAADzLExG6uCfgABl0UQFLI2ny9wvY8wAATndl-8tzyDq9zyAAIC', 'AgADBAADyLExG6uCfgABiop-lux6czIfQYswAASIWlelkQEKZedyAQABAg', 'AgADBAADzLExG6uCfgABl0UQFLI2ny9wvY8wAATndl-8tzyDq9zyAAIC');
+                    $file_id= Utils::aleatorio(array('AgADBAADyrExG6uCfgAByJa4e096PDrUuqYwAAT4WookikriBOAAIC', 'AgADBAADzLExG6uCfgABl0UQFLI2ny9wvY8wAATndl-8tzyDq9zyAAIC', 'AgADBAADyLExG6uCfgABiop-lux6czIfQYswAASIWlelkQEKZedyAQABAg', 'AgADBAADzLExG6uCfgABl0UQFLI2ny9wvY8wAATndl-8tzyDq9zyAAIC'));
                     return Response::create_photo_response($endpoint, $request->get_chat_id(), $file_id);
                     break;
                 default:
