26421b2db0de6b7c927b36e3a9717cd90344113f
ketu84 <ketu84@gmail.com>
2016-10-01 13:04:29 +0000

    Resultados por paises

--- limb/ComandosOffTopic.php
+++ limb/ComandosOffTopic.php
@@ -419,10 +419,39 @@
             $params = $request->get_command_params();
             $numparams = count($params);
 			
-			$fecha = ($numparams == 0) ? date('Y-m-d') : date('Y-m-d', strtotime($params[0]));
+			if($numparams > 0 && $numparams <=2)
+			{
+			    $fecha = date('Y-m-d');
+			    $competicion = "Champions";
+			    if(Utils::IsDate($params[0]))
+			        $fecha = $params[0];
+			    else 
+			        $competicion = $params[0];
+			        
+                if(Utils::IsDate($params[1]))
+			        $fecha=$params[1];
+                else 
+			        $competicion = $params[1];
+			}
+			$idCompeticion = 440; // por defecto, la champions
+			switch(strtolower($competicion))
+			{
+                case 'inglaterra'. $idCompeticion = 426; break;
+                case 'inglaterra2': $idCompeticion = 427; break;
+                case 'alemania': $idCompeticion = 430; break;
+                case 'holanda': $idCompeticion = 433; break;
+                case 'españa': $idCompeticion = 436; break;
+                case 'españa2': $idCompeticion = 437; break;
+                case 'francia': $idCompeticion = 434; break;
+                case 'francia2': $idCompeticion = 435; break;
+                case 'italia': $idCompeticion = 438; break;
+                case 'portugal': $idCompeticion = 439; break;
+                default: $competicion = 'champions';
+			}
+			$competicion = str_replace('2', ' 2', ucfirst($competicion));
 			$fechaFormateada = date('d/m/Y', strtotime($fecha));
-			$text='*Resultados ('.$fechaFormateada.'):*'.PHP_EOL;
-			$apiurl = 'http://api.football-data.org/v1/competitions/440/fixtures';
+			
+			$apiurl = 'http://api.football-data.org/v1/competitions/'.$idCompeticion.'/fixtures';
 			$content = file_get_contents($apiurl);
 			$json = json_decode($content, true);
 			foreach($json['fixtures'] as $item) {
@@ -441,9 +470,15 @@
 					}
 				}
 			}
+			if(isset($text))
+			    $text ='*Resultados '.$competicion.' ('.$fechaFormateada.'):*'.PHP_EOL.$text;
+			else 
+			    $text ='*No hay partidos para '.$competicion.' ('.$fechaFormateada.')*';
+			
 			$response = new Response($endpoint, $request->get_chat_id(), Response::TYPE_TEXT);
             $response->text=$text;
             $response->markdown=true;
+            
 	
 		    $this->log->debug("Fin Resultados (".(microtime(true)-$time)." s): ");
             return $response;
