77434d8f4805c993c3262df53b4cf6b8a2dbe565
Antonio Tapia <altapia@gmail.com>
2016-10-05 07:52:54 +0000

    respuestas bot

--- limb/ComandosOffTopic.php
+++ limb/ComandosOffTopic.php
@@ -65,13 +65,13 @@
                 $file_id = 'BQADBAAD7AAD-WxHAtGrH8UmWiiXAg';
                 return Response::create_sticker_response($endpoint, $request->get_chat_id(), $file_id);
             }
-            if (strpos($request->get_command(),'bot') !== false) {
-                $audio_id='BQADBAADZgEAAphMPgAB-gq0Aunt4koC';
-                return Response::create_audio_response($endpoint, $request->get_chat_id(), $audio_id);
-            }
             if (strpos($request->get_command(),'bot') !== false && strpos($request->get_command(),'noches') !== false) {
                 $audio_id='BQADBAADaAEAAphMPgABFMy-yOPil3AC';
                 return Response::create_audio_response($endpoint, $request->get_chat_id(), $file_id);
+            }
+            if (strpos($request->get_command(),'bot') !== false) {
+                $audio_id='BQADBAADZgEAAphMPgAB-gq0Aunt4koC';
+                return Response::create_audio_response($endpoint, $request->get_chat_id(), $audio_id);
             }
             
             return $this->insultar($endpoint, $request);
