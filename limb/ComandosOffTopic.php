cb7d34e546022e2397ae737bb4aec9b7eb0066bb
Antonio Tapia <altapia@gmail.com>
2016-10-05 07:53:43 +0000

    respuestas bot

--- limb/ComandosOffTopic.php
+++ limb/ComandosOffTopic.php
@@ -67,7 +67,7 @@
             }
             if (strpos($request->get_command(),'bot') !== false && strpos($request->get_command(),'noches') !== false) {
                 $audio_id='BQADBAADaAEAAphMPgABFMy-yOPil3AC';
-                return Response::create_audio_response($endpoint, $request->get_chat_id(), $file_id);
+                return Response::create_audio_response($endpoint, $request->get_chat_id(), $audio_id);
             }
             if (strpos($request->get_command(),'bot') !== false) {
                 $audio_id='BQADBAADZgEAAphMPgAB-gq0Aunt4koC';
